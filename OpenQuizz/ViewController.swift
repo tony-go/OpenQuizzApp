fffabd9834253310ce3b1c5c3742ca9c0cab4fee
tony-go <gorez.tony@gmail.com>
2021-04-12 08:29:38 +0200

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Gorez Tony on 12/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
